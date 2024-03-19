989472f41060f804ce669ee1a5de19e17f94b6d1
MariannaHon <148065963+MariannaHon@users.noreply.github.com>
2024-03-19 21:09:12 +0000

    Deploying to gh-pages from @ MariannaHon/goit-js-hw-11@b46914767e1a018df8170c0c7c843d5c8b6b5454 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,12 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();
+import"./assets/vendor-db7463ae.js";(function(){const n=document.createElement("link").relList;if(n&&n.supports&&n.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&r(a)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();function l(){const o={method:"GET"},s=`https://pixabay.com/api/?${new URLSearchParams({key:"42957626-41f27679caf00334274850a6e",q:i.word.value,image_type:"photo",orientation:"horizontal",safesearch:"true"})}`;return fetch(s,o).then(r=>{if(!r.ok)throw new Error(r.status);return r.json()})}function c(){return i.gallery.innerHTML="",`<li class="gallery-item">
+                <a class="gallery-link" href="${largeImageURL}">
+                  <img class="gallery-image" src="${webformatURL}" alt="${tags}" />
+                </a>
+                <div class="stats">
+                  <p>Likes<br/>${likes}</p>
+                  <p>Views<br/>${views}</p>
+                  <p>Commens<br/>${comments}</p>
+                  <p>Downloads<br/>${downloads}</p>
+                </div>
+              </li>`}const i={gallery:document.querySelector(".gallery"),form:document.querySelector(".form"),word:document.querySelector(".input")};i.form.addEventListener("submit",o=>{o.preventDefault(),o.target.elements.imgInput.value,l().then(n=>{const s=c();i.gallery.insertAdjacentHTML("beforeend",s)})});
 //# sourceMappingURL=commonHelpers.js.map
